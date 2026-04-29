7f4691dbd9b3f083a88fe510f8cc5cb3fff6eb24
GrafiMundi <aballesterosn@correo.unicordoba.edu.co>
2026-04-28 21:39:42 -0500

    Interfaz principal de la tienda virtual (Login)

---
+++ src/controller/GrafiMundiViewController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.StackPane;
+import javafx.scene.control.Button;
+
+public class GrafiMundiViewController {
+
+    @FXML
+    private StackPane containerForm;
+
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    private Button btnRegistro;
+
+}
